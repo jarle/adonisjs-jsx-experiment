27a0c49173925ab2ff9df1c8402d8f6f80d0610d
Jarle Mathiesen <jarle.mathiesen@gmail.com>
2025-06-15 16:51:17 +0200

    commit

--- app/pages/route.tsx
+++ app/pages/route.tsx
@@ -2,10 +2,10 @@ import { defineRoute } from "../../lib/routeModule.js"
 
 export default defineRoute({
   async loader() {
-    return { hello: 'world' }
+    return { message: 'world' }
   },
 
   view({ loaderData }) {
-    return <div>Hello from component requested at {loaderData?.hello}</div>
+    return <div>Hello from component requested at {loaderData.message}</div>
   },
 })
