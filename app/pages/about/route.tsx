a9f64d57dfcfe3a81f5252ea7c78eb4e12fc65eb
Jarle Mathiesen <jarle.mathiesen@gmail.com>
2025-06-15 16:47:34 +0200

    enable csrf

--- app/pages/about/route.tsx
+++ app/pages/about/route.tsx
@@ -1,4 +1,5 @@
 import logger from "@adonisjs/core/services/logger";
+import { csrfField } from "adonisjsx";
 import { defineRoute } from "../../../lib/routeModule.js";
 
 export default defineRoute({
@@ -8,6 +9,7 @@ export default defineRoute({
   view() {
     return (
       <form method="POST">
+        {csrfField()}
         <input name="email"></input>
         <button>Submit</button>
       </form>
